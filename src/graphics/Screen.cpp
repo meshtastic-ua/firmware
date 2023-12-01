de8c889870a7e12b1ce34ae2e95f46aeab836a1d
UT3UKU <serhii.bondarenko@gmail.com>
2023-12-02 00:28:37 +0200

    add ua logo text

--- src/graphics/Screen.cpp
+++ src/graphics/Screen.cpp
@@ -159,7 +159,7 @@ static void drawIconScreen(const char *upperMsg, OLEDDisplay *display, OLEDDispl
     display->setFont(FONT_MEDIUM);
     display->setTextAlignment(TEXT_ALIGN_LEFT);
     //const char *title = "meshtastic.org";
-    const char *title = "GLORY  UKRAINE";
+    const char *title = "UKRAINE";
     display->drawString(x + getStringCenteredX(title), y + SCREEN_HEIGHT - FONT_HEIGHT_MEDIUM, title);
     display->setFont(FONT_SMALL);
 
