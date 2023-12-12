a47d62409b048dcb4867673f2fab9260fc3cdd5a
Yehor Nazarkin <nimnull@gmail.com>
2023-12-13 01:54:28 +0200

    Increased smart position min distance broadcast to 1km. Set min interval for position broadcast to 1min. Smart position broadcast disabled by default

--- src/mesh/NodeDB.cpp
+++ src/mesh/NodeDB.cpp
@@ -178,9 +178,9 @@ void NodeDB::installDefaultConfig()
     config.device.disable_triple_click = true;
 #endif
     config.position.gps_enabled = true;
-    config.position.position_broadcast_smart_enabled = true;
-    config.position.broadcast_smart_minimum_distance = 100;
-    config.position.broadcast_smart_minimum_interval_secs = 30;
+    config.position.position_broadcast_smart_enabled = false;
+    config.position.broadcast_smart_minimum_distance = 1000;
+    config.position.broadcast_smart_minimum_interval_secs = 60;
     if (config.device.role != meshtastic_Config_DeviceConfig_Role_ROUTER)
         config.device.node_info_broadcast_secs = 3 * 60 * 60;
     config.device.serial_enabled = true;
